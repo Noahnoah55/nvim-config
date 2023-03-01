41c0b0fbbccb575060fca1772e2022d02a9f67d1
nehrman <noah.a.ehrman@gmail.com>
2023-03-01 12:02:48 -0500

    More fugitive stuff

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pw', builtin.lsp_references, {})
